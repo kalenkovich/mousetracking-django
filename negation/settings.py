823612fdd1739d9e3c1d28fb7d08ec295a198ac8
kalenkovich <e.kalenkovich@gmail.com>
2020-05-15 08:27:59 +0300

    Added pythonanywhere as host

--- negation/settings.py
+++ negation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f6@!ys@tvly^1=qfd^9@v5%9688o4f%p!s$k@m+83brq%hd%w-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
