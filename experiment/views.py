873606a8dd893654c6e86d60af8e7f32bd7af4fb
kalenkovich <e.kalenkovich@gmail.com>
2020-05-15 13:19:36 +0300

    Route to "mousetracking" once welcome has been seen

--- experiment/views.py
+++ experiment/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def router(request):
     """
     This view routes to all the other ones depending on the stage the participant is at
     """
-    return welcome(request)
+    saw_welcome = request.COOKIES.get("saw-welcome")
+    if not saw_welcome:
+        return welcome(request)
+    else:
+        return mousetracking(request)
 
 
 def welcome(request):
     return render(request, 'experiment/welcome.html')
+
+
+def mousetracking(request):
+    return HttpResponse('The mousetracking will happen here')
