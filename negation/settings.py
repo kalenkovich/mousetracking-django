6aa8389891f916cb648399f1b485cde3e195a5cf
kalenkovich <e.kalenkovich@gmail.com>
2020-05-25 13:42:29 +0300

    fix: access secret_key.txt independent of working directory
    I used `open('secret_key.txt')` which works locally (with `manage.py runserver`) but not on PythonAnywhere.
    It probably works locally because 'manage.py' is in the same directory as 'secret_key.txt'.

--- negation/settings.py
+++ negation/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret_key.txt') as f:
+secret_key_path = os.path.join(BASE_DIR, 'secret_key.txt')
+with open(secret_key_path) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
