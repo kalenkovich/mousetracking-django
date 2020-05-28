a2daa6a0c946cb7c6cd08e6a69429cd6ea4695d4
kalenkovich <e.kalenkovich@gmail.com>
2020-05-28 07:56:32 +0300

    actually, save the results if we can recover the trial

--- experiment/views.py
+++ experiment/views.py
@@ -56,6 +56,26 @@ def save_trial_results(request):
     results = json.loads(request.body.decode('utf-8')).get('results')
     participant: Participant = Participant.get_participant(request)
     trial: Trial = participant.get_last_sent_trial()
-    if trial is not None and results.get('trial_id') == trial.unique_id:
-        trial.save_results(results)
+
+    # Trial might be None if another participant is using the same browser by clearing cookies without clearing local
+    # storage which contained trial setting from the last unrun trial of the previous participant. This is very unlikely
+    # to happen outside of the development situation but we don't want to get an error. In this case, we just proceed to
+    # sending the first actual trial.
+    if trial is not None:
+        trial_id_received = results.get('trial_id')
+        if trial_id_received == trial.unique_id:
+            # This should be the most common case: the received results correspond to the last trial sent.
+            trial.save_results(results)
+        else:
+            # If the trial corresponding to the results belongs to the correct participant and we had not received the
+            # result previously, we will save the results. This can happen if we already sent a new trial in parallel,
+            # while dealing with saving the results.
+            try:
+                trial = participant.trial_set.get(unique_id=trial_id_received)
+                if not hasattr(trial, 'trialresults'):
+                    trial.save_results(results)
+            except Trial.DoesNotExist:
+                # We got results from a rogue trial, probably from a different attempt on the same computer.
+                pass
+
     return get_new_trial_settings(request, participant=participant)
