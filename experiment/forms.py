cb02fe0ddb4b17033e0e9e7d9be2fde3794d7379
kalenkovich <e.kalenkovich@gmail.com>
2020-06-04 21:40:40 +0300

    made the consent checkbox obligatory

--- experiment/forms.py
+++ experiment/forms.py
@@ -1,8 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 from .models import Participant
 
 
 class ParticipantForm(ModelForm):
+    gave_consent = BooleanField(required=True)
+
     class Meta:
         model = Participant
         fields = ['age', 'sex', 'gave_consent']
