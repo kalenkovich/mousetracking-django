257b67c2c9feb363a1755d5a854f66af25ce8537
kalenkovich <e.kalenkovich@gmail.com>
2020-06-19 16:12:27 +0300

    use a list of sound urls from the server
    Not the one hardcoded in the 'HeadphoneCheck.js'. It is not any less hardcoded in the view, of course. The point of this is to then change the urls to point to the files on our server instead of the McDermott's lab's S3 bucket. The problem with the bucket files is that one of the files is corrupted.

--- experiment/urls.py
+++ experiment/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'experiment'
+
 urlpatterns = [
     path('', views.router, dict(is_test=False), name='router'),
     path('test/', views.router, dict(is_test=True), name='router_test'),
     re_path(r'(?:test/)?get_new_trial_settings/', views.get_new_trial_settings, name='get_new_trial_settings'),
     re_path(r'(?:test/)?save_trial_results/', views.save_trial_results, name='save_trial_results'),
+    path('headphone_check_json', views.headphone_check_json, name='headphone_check_json')
 ]
--- experiment/views.py
+++ experiment/views.py
@@ -158,3 +158,24 @@ def participant_form(request, participant=None):
 
 def devices_questionnaire_form(request, participant=None):
     return abstract_form(request, participant, DevicesQuestionnaireForm, 'experiment/devices_questionnaire_form.html')
+
+
+def headphone_check_json(request):
+    data = {
+        'stimuli': [
+            {'id': 1, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_ISO.wav',
+             'correct': '2'},
+            {'id': 2, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_IOS.wav',
+             'correct': '3'},
+            {'id': 3, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_SOI.wav',
+             'correct': '1'},
+            {'id': 4, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_SIO.wav',
+             'correct': '1'},
+            {'id': 5, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_OSI.wav',
+             'correct': '2'},
+            {'id': 6, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_OIS.wav',
+             'correct': '3'}
+        ],
+        'calibration': {'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/noise_calib_stim.wav'}
+    }
+    return JsonResponse(data=data)
