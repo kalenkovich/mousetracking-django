42fcc9f4300ca728c446c1d5138b500a9750985d
kalenkovich <e.kalenkovich@gmail.com>
2020-06-19 16:30:26 +0300

    switched to local copies of the test sounds

--- experiment/views.py
+++ experiment/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.templatetags.static import static
 from django.urls import reverse
 
 from .forms import ParticipantForm, DevicesQuestionnaireForm
@@ -161,21 +162,22 @@ def devices_questionnaire_form(request, participant=None):
 
 
 def headphone_check_json(request):
+    wav_folder = 'experiment/headphone_check/'
+
+    def get_wav_url(wav_name):
+        return static(wav_folder + wav_name)
+
     data = {
         'stimuli': [
-            {'id': 1, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_ISO.wav',
-             'correct': '2'},
-            {'id': 2, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_IOS.wav',
-             'correct': '3'},
-            {'id': 3, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_SOI.wav',
-             'correct': '1'},
-            {'id': 4, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_SIO.wav',
-             'correct': '1'},
-            {'id': 5, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_OSI.wav',
-             'correct': '2'},
-            {'id': 6, 'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/antiphase_HC_OIS.wav',
-             'correct': '3'}
+            {'id': 1, 'src': get_wav_url('antiphase_HC_ISO.wav'), 'correct': '2'},
+            {'id': 2, 'src': get_wav_url('antiphase_HC_IOS.wav'), 'correct': '3'},
+            {'id': 3, 'src': get_wav_url('antiphase_HC_SOI.wav'), 'correct': '1'},
+            {'id': 4, 'src': get_wav_url('antiphase_HC_SIO.wav'), 'correct': '1'},
+            {'id': 5, 'src': get_wav_url('antiphase_HC_OSI.wav'), 'correct': '2'},
+            {'id': 6, 'src': get_wav_url('antiphase_HC_OIS.wav'), 'correct': '3'}
         ],
-        'calibration': {'src': 'https://s3.amazonaws.com/mcd-headphone-check/v1.0/assets/noise_calib_stim.wav'}
+        'calibration':
+            {'src': get_wav_url('noise_calib_stim.wav')}
     }
+
     return JsonResponse(data=data)
