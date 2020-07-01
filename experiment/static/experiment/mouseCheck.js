8452ebe0d2e8e00e3be9200c9d65ad6adb6bd5e6
kalenkovich <e.kalenkovich@gmail.com>
2020-07-01 17:55:55 +0300

    set up the check without actually checking

---
+++ experiment/static/experiment/mouseCheck.js
@@ -0,0 +1,35 @@
+const mouseCheck = {
+    runMouseCheck: function(){
+        mouseCheck.render();
+    },
+
+    render: function(){
+        const container = $('#hc-container');
+
+        // Instruction
+        $('<hr/>').appendTo(container);
+        $('<div/>', {
+          class: 'mouseCheck-instruction',
+          // text: 'To check the mouse, scroll using the wheel in the window below until the "Continue" button appears'
+          text: 'Для проверки работы мыши прокрутите колесиком окошко снизу, пока не появится кнопка "Продолжить"'
+        }).appendTo(container);
+
+        // Scrollable window with the hidden "continue" button.
+
+        // Add button to continue
+        $('<br/>').appendTo(container);
+        $('<button/>', {
+          id: 'mouseCheck-continue-button',
+          class: 'mouseCheck-instruction btn btn-primary',
+          disabled: false,  // TODO: enable after at least some scrolling
+          // text: 'Continue',
+          text: 'Продолжить',
+          click: function () {
+            container.empty();
+            $(document).trigger('mouseCheckEnd', {didPass: true});
+          }
+        }).appendTo(container);
+    },
+
+
+};
